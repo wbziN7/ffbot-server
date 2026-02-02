15f97fbd46b5938c5dd26f1fc2ef48b0319327f8
wbziN7 <131833395+wbziN7@users.noreply.github.com>
2026-02-02 17:43:06 -0300

    Update servidor_licencas.py

--- servidor_licencas.py
+++ servidor_licencas.py
@@ -8,7 +8,7 @@ import json
 app = Flask(__name__)
 
 # Senha do admin (MUDE ISSO!)
-ADMIN_PASSWORD = "admin123"
+ADMIN_PASSWORD = "Wb03122008!"
 
 # =====================================================
 # BANCO DE DADOS
