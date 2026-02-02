24498fae4bb720945f54642634f5fc5c9bc5dc92
wbziN7 <131833395+wbziN7@users.noreply.github.com>
2026-02-02 17:26:11 -0300

    Update server.py

--- server.py
+++ server.py
@@ -319,5 +319,5 @@ async function stats(){
 
 if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
